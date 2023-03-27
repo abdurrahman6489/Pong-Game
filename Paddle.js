5557e8fb90d2f51c833b1d9d47415d9da265cde8
abdurrahman6489 <abdurrahman489@gmail.com>
2023-03-28 01:02:14 +0530

    pushing the code for Pong Game

--- Paddle.js
+++ Paddle.js
@@ -13,4 +13,16 @@ export default class Paddle{
     update(delta,ballHeight){
         this.position += delta * SPEED * (ballHeight - this.position);
     }
+    reset(){
+        this.position = 50;
+    }
+    rect(){
+        return this.paddleElem.getBoundingClientRect();
+    }
+    get height(){
+        return parseFloat(getComputedStyle(this.paddleElem).getPropertyValue("--height"));
+    }
+    set height(value){
+        this.paddleElem.style.setProperty("--height",value);
+    }
 }
